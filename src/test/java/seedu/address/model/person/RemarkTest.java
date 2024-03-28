db3c143e344e03effd449d58a8f015ed818aa552
tahnya <111273056+tahnya@users.noreply.github.com>
2024-03-28 23:54:37 +0800

    Update tests

--- src/test/java/seedu/address/model/person/RemarkTest.java
+++ src/test/java/seedu/address/model/person/RemarkTest.java
@@ -1,58 +1,31 @@
 package seedu.address.model.person;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 public class RemarkTest {
 
     @Test
-    public void constructor_null_throwsNullPointerException() {
-        assertThrows(NullPointerException.class, () -> new Remark(null));
-    }
-
-    @Test
-    public void constructor_invalidRemark_throwsIllegalArgumentException() {
-        String invalidRemark = "  ";
-        assertThrows(IllegalArgumentException.class, () -> new Remark(invalidRemark));
-    }
-
-    @Test
-    public void isValidRemark() {
-        // null remark
-        assertThrows(NullPointerException.class, () -> Remark.isValidRemark(null));
-
-        // invalid remarks
-        assertFalse(Remark.isValidRemark("     ")); // spaces only
-        assertFalse(Remark.isValidRemark(" ")); // spaces only
-
-        // valid remarks
-        assertTrue(Remark.isValidRemark("")); // empty remark if not entered
-        assertTrue(Remark.isValidRemark("Random remark testing"));
-        assertTrue(Remark.isValidRemark("-")); // one character
-        assertTrue(Remark.isValidRemark("Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
-                "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")); // long remark
-    }
-
-    @Test
     public void equals() {
-        Remark remark = new Remark("Valid Remark");
-
-        // same values -> returns true
-        assertTrue(remark.equals(new Remark("Valid Remark")));
+        Remark remark = new Remark("Hello");
 
         // same object -> returns true
         assertTrue(remark.equals(remark));
 
+        // same values -> returns true
+        Remark remarkCopy = new Remark(remark.value);
+        assertTrue(remark.equals(remarkCopy));
+
+        // different types -> returns false
+        assertFalse(remark.equals(1));
+
         // null -> returns false
         assertFalse(remark.equals(null));
 
-        // different types -> returns false
-        assertFalse(remark.equals(5.0f));
-
-        // different values -> returns false
-        assertFalse(remark.equals(new Remark("Other Valid Remark")));
+        // different remark -> returns false
+        Remark differentRemark = new Remark("Bye");
+        assertFalse(remark.equals(differentRemark));
     }
 }
