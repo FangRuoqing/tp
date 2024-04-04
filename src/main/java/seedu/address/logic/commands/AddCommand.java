17905af563a3b9e1205ce63ab5a932e4a0a1a3ba
yapxuanxuan <e0969213@u.nus.edu>
2024-04-04 14:52:46 +0800

    Minor updates to Message Usages

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -34,7 +34,6 @@ public class AddCommand extends Command {
             + PREFIX_PHONE + "98765432 "
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
-            + PREFIX_REMARK + ""
             + PREFIX_TAG + "friends "
             + PREFIX_TAG + "owesMoney";
 
